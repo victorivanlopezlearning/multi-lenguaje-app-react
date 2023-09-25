a90ea4f3d51e6d6a81fcea929f5ed0e0b7dc3064
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-25 13:50:51 -0600

    Added: FormattedMessage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,53 @@
 import { NavLink } from 'react-router-dom';
 import es from './../assets/img/mexico.png';
 import en from './../assets/img/usa.png';
+import { FormattedMessage } from 'react-intl';
 
 export const Navbar = () => {
   return (
-      <div className="navbar">
-        <nav className="nav">
-          <NavLink
-            className={({ isActive }) => `${isActive ? 'active' : ''}`}
-            to="/"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => `${isActive ? 'active' : ''}`}
-            to="/explore"
-          >
-            Explore
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => `${isActive ? 'active' : ''}`}
-            to="/profile"
-          >
-            Profile
-          </NavLink>
-        </nav>
+    <div className="navbar">
+      <nav className="nav">
+        <NavLink
+          className={({ isActive }) => `${isActive ? 'active' : ''}`}
+          to="/"
+        >
+          <FormattedMessage
+            id='Navbar.home'
+            defaultMessage='Home'
+          />
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => `${isActive ? 'active' : ''}`}
+          to="/explore"
+        >
+          <FormattedMessage
+            id='Navbar.explore'
+            defaultMessage='Explore'
+          />
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => `${isActive ? 'active' : ''}`}
+          to="/profile"
+        >
+          <FormattedMessage
+            id='Navbar.profile'
+            defaultMessage='Profile'
+          />
+        </NavLink>
+      </nav>
 
-        <div className="flags">
-          <button
-            // onClick={() => idioma.establecerLenguaje('es-MX')}
-          >
-            <img src={es} alt="Imagen bandera" />
-          </button>
-          <button 
-            // onClick={() => idioma.establecerLenguaje('en-US')}
-          >
-            <img src={en} alt="Imagen bandera" />
-          </button>
-        </div>
+      <div className="flags">
+        <button
+        // onClick={() => idioma.establecerLenguaje('es-MX')}
+        >
+          <img src={es} alt="Imagen bandera" />
+        </button>
+        <button
+        // onClick={() => idioma.establecerLenguaje('en-US')}
+        >
+          <img src={en} alt="Imagen bandera" />
+        </button>
       </div>
+    </div>
   )
 }
