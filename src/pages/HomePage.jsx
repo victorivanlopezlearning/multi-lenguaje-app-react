29f2b0f4ba35f4e32bff94680105f43f0643fa5c
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-25 14:36:11 -0600

    Added: FormattedDate and FormattedMessage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,25 @@
+import { FormattedDate, FormattedMessage } from 'react-intl';
 
 export const HomePage = () => {
   return (
-    <h1>Home Page</h1>
+    <>
+      <h1 className="title">
+        <FormattedMessage
+          id='Home.title'
+          defaultMessage='Welcome'
+          values={{
+            name: 'Víctor Iván López'
+          }}
+        />
+      </h1>
+      <p className="subtitle">
+        <FormattedDate
+          value={Date.now()}
+          year="numeric"
+          month="long"
+          day="numeric"
+        />
+      </p>
+    </>
   )
 }
